6d48322ff4161ee7377f271388a460259aa26218
stellakaranikola <karanikolastella@hotmail.com>
2019-12-04 21:41:30 +0200

    implement login, register and home page, Implement script to create the database, implement simple css

---
+++ createDataBaseScript.php
@@ -0,0 +1,18 @@
+<?php
+include('server.php');
+
+$sql = "CREATE TABLE users (
+  id int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  password varchar(100) NOT NULL
+)";
+
+
+if ($db->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $db->error;
+}
+
+?>
